14d82b0be3f0fefb63ed06c1a73517abf7261a05
jaysunxiao <jaysunxiao@gmail.com>
2025-08-18 16:58:41 +0800

    update

--- spider/ths-spider-all.mjs
+++ spider/ths-spider-all.mjs
@@ -336,7 +336,7 @@ try {
     day = day.toString().padStart(2, '0');
     let formatTime = year + month + day;
     // let formatTime = "20250115";
-    await spiderIndexes("2508", formatTime);
+    await spiderIndexes("2509", formatTime);
 } catch (error) {
     console.log('zfoo_error', error);
 } finally {
