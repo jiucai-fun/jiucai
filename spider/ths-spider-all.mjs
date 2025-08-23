feb9933a87440bd61d0d268f0a79f68bc8d425bc
jaysunxiao <jaysunxiao@gmail.com>
2025-08-23 21:37:16 +0800

    update

--- spider/ths-spider-all.mjs
+++ spider/ths-spider-all.mjs
@@ -1,7 +1,7 @@
 import {connect} from "puppeteer-real-browser";
 import Papa from 'papaparse';
 import fs from 'fs';
-
+import { loginThs } from './ths-utils.mjs';
 
 import {delay} from './websocket.mjs';
 
@@ -36,30 +36,6 @@ let {browser, page} = await connect({
 });
 const context = browser.defaultBrowserContext();
 await context.overridePermissions(loginUrl, ['clipboard-read', 'clipboard-write', 'clipboard-sanitized-write']);
-// 登录同花顺
-const loginThs = async () => {
-    await page.goto(loginUrl, {waitUntil: 'networkidle0'});
-
-    // 如果是周一记得提示重新登录，因为同花顺的cookie有效期1周
-    const date = new Date();
-    if (date.getDay() === 1) {
-        for (let i = 0; i < 18; i++) {
-            console.warn("同花顺的cookie有效期1周，周一记得重新登录同花顺账号");
-            await delay(1000);
-        }
-    }
-
-    // 如果没有登录按钮则表示已经登录成功
-    while (true) {
-        const loginHideButton = await page.$('>>> [class="login-box hide"]');
-        if (loginHideButton != null) {
-            console.warn("同花顺账号已登录！！！！！！！");
-            return;
-        }
-        console.error("请登录同花顺账号");
-        await delay(5 * 1000);
-    }
-}
 
 const spiderStocks = async () => {
     // 记录函数开始时间
@@ -243,7 +219,7 @@ const spiderOptions = async (optionIndex, date) => {
 
 
 try {
-    await loginThs();
+    await loginThs(page);
 
     // 爬取股票
     await spiderStocks();
---
+++ spider/ths-utils.mjs
@@ -0,0 +1,29 @@
+import {connect} from "puppeteer-real-browser";
+import {delay} from './websocket.mjs';
+
+const loginUrl = "https://q.10jqka.com.cn/";
+
+// 登录同花顺
+export const loginThs = async (page) => {
+    await page.goto(loginUrl, {waitUntil: 'networkidle0'});
+
+    // 如果是周一记得提示重新登录，因为同花顺的cookie有效期1周
+    const date = new Date();
+    if (date.getDay() === 1) {
+        for (let i = 0; i < 18; i++) {
+            console.warn("同花顺的cookie有效期1周，周一记得重新登录同花顺账号");
+            await delay(1000);
+        }
+    }
+
+    // 如果没有登录按钮则表示已经登录成功
+    while (true) {
+        const loginHideButton = await page.$('>>> [class="login-box hide"]');
+        if (loginHideButton != null) {
+            console.warn("同花顺账号已登录！！！！！！！");
+            return;
+        }
+        console.error("请登录同花顺账号");
+        await delay(5 * 1000);
+    }
+}
